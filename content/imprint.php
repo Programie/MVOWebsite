65c3a53632d80840a769943e9c81255896b688cb
programie <github@selfcoders.com>
2013-02-23 13:01:48 +0000

    Added imprint page Set fixed height for picture list items (Fixes almost empty lines)

---
+++ content/imprint.php
@@ -0,0 +1,42 @@
+<?php
+if (Constants::$getPageTitle)
+{
+	$title = "Impressum";
+	return;
+}
+?>
+<h1>Impressum</h1>
+<p>
+	Diese Seiten wurden erstellt vom Musikverein "Orgelfels" Reichental e.V.<br />
+	Hinweise und Fragen k&ouml;nnen Sie gerne an <a href="mailto:webmaster@musikverein-reichental.de">webmaster@musikverein-reichental.de</a> richten.
+</p>
+
+<h2>Haftungsausschluss</h2>
+Wir bem&uuml;hen uns um aktuelle, korrekte und vollst&auml;ndige Informationen, k&ouml;nnen aber keine Haftung f&uuml;r die Richtigkeit, Vollst&auml;ndigkeit und Aktualit&auml;t der Ver&ouml;ffentlichungen und Links &uuml;bernehmen.
+Ebenso kann es durch die Digitalisierung von Daten, den Einsatz unterschiedlicher Browser und aufgrund unterschiedlicher Software-Einstellungen bei der Darstellung von Bildern und Farben zu Abweichungen kommen, auf die wir keinen Einfluss haben.
+Wir sind nicht verantwortlich f&uuml;r die Inhalte externer Internetseiten.
+
+<h2>Urheberrechtshinweise</h2>
+Die in dieser Website enthaltenen Bilder und Informationen werden von Michael Wieland erstellt oder mit der Genehmigung der Rechteinhaber verwendet.
+Eine vom Urheberrechtsgesetz nicht zugelassene Weiterverwendung in gedruckter oder elektronischer Form der Daten und Bilder setzt die vorherige schriftliche Genehmigung voraus.
+Andere hier erw&auml;hnte Produkte oder Unternehmen k&ouml;nnen eingetragene Marken, gesch&auml;ftliche Bezeichnungen oder Zeichen der jeweiligen Rechteinhaber sein.
+
+<h2>Haftung f&uuml;r Links</h2>
+Mit Urteil vom 12. Mai 1998 hat das Landgericht Hamburg entschieden, dass mit Anbringung eines Links die Inhalte der gelinkten Seite ggf. mit zu verantworten sind.
+Dies kann - so das Landgericht - ausschlie&szlig;lich dadurch verhindert werden, dass man sich von diesen Inhalten ausdr&uuml;cklich distanziert.
+Wir haben Links zu anderen Seiten im Internet gelegt.
+F&uuml;r alle diese Links gilt: Wir bem&uuml;hen uns um Sorgfalt bei der Auswahl der Verlinkung, haben aber selbst keinerlei Einfluss auf die Inhalte oder die Gestaltung der gelinkten Seiten.
+Wir &uuml;bernehmen keinerlei Verantwortung f&uuml;r die Inhalte von Internetseiten, auf deren URL in unseren Seiten verwiesen wird.
+Diese Erkl&auml;rung gilt f&uuml;r alle auf unseren Seiten angebrachten Links und f&uuml;r die Inhalte aller Seiten, zu denen die angebrachten Links f&uuml;hren.
+
+<h2>Erl&auml;uterung zum Datenschutz</h2>
+Bei der Beantwortung Ihrer Fragen ist es erforderlich, Sie nach pers&ouml;nlichen Daten zu fragen.
+Wir werden nach den Regelungen des Bundesdatenschutzgesetzes (BDSG) damit umgehen und nicht ohne Ihre Erlaubnis Ihre Daten an Dritte weitergeben.
+M&ouml;chten Sie nicht, dass Ihre Daten f&uuml;r eine weitere Kontaktaufnahme verwendet werden und/oder sollen die Daten gel&ouml;scht werden, so teilen Sie uns dies bitte mit, wir werden Ihrem Wunsch entsprechen.
+Technische Informationen wie Ihre Internet-Protokolladresse, das Betriebssystem und der Browsertyp werden ausschlie&szlig;lich zu internen statistischen Zwecken verwendet und unterliegen ebenso dem Datenschutz.
+
+<h2>Informationen zu Cookies</h2>
+In einigen Bereichen werden Cookies benutzt, die Informationen auf Ihrer Festplatte speichern.
+Die gespeicherten Informationen erh&ouml;hen den Bedienungskomfort der Webseite.
+Wenn Sie dies nicht m&ouml;chten, k&ouml;nnen Sie die Cookies in Ihrem Browser verbieten.
+Dadurch k&ouml;nnen eventuell Einschr&auml;nkungen der Bedienbarkeit entstehen.
