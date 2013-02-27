b24a0810ae1a721942d958fb3bfc71a4e7b8d720
programie <github@selfcoders.com>
2013-02-27 23:01:37 +0000

    Added page "musiker" Fixed showing arrow in wrong direction in headers of sortable tables

---
+++ content/musiker.php
@@ -0,0 +1,39 @@
+<?php
+if (Constants::$getPageTitle)
+{
+	$title = "Musiker";
+	return;
+}
+
+$groups = array();
+$users = array();
+
+$query = Constants::$pdo->query("SELECT `musiciangroups`.`name` AS `groupName`, `musiciangroups`.`title` AS `groupTitle`, `users`.`firstName`, `users`.`lastName`, `users`.`id` AS `userId` FROM `users` LEFT JOIN `musiciangroups` ON `musiciangroups`.`id` = `users`.`musicianGroupId` WHERE `users`.`musicianGroupId` ORDER BY `musiciangroups`.`orderIndex` ASC, `musiciangroups`.`title` ASC, `users`.`lastname` ASC, `users`.`firstName` ASC");
+while ($row = $query->fetch())
+{
+	$groups[$row->groupName] = $row->groupTitle;
+	$users[$row->groupName][] = $row;
+}
+
+echo "<h1>Musiker</h1>";
+
+foreach ($groups as $groupName => $groupTitle)
+{
+	echo "<h2>" . $groupTitle . "</h2>";
+	
+	echo "<ul class='polaroids'>";
+	foreach ($users[$groupName] as $user)
+	{
+		echo "
+			<li>
+				<a href='/files/musiker/" . $user->userId . ".jpg' rel='lightbox' caption='" . $user->firstName . " " . $user->lastName . "'>
+					<img src='/files/musiker/" . $user->userId . ".jpg'/>
+				</a>
+			</li>
+		";
+	}
+	echo "</ul>";
+	
+	echo "<div class='clear'></div>";
+}
+?>
