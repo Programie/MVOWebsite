cb5681c55399bb36426808c2d5d0c0c63e95e930
programie <github@selfcoders.com>
2013-03-04 23:22:19 +0000

    Added workaround for slimbox bug "Overlay over image"

--- files/scripts/slimbox.js
+++ files/scripts/slimbox.js
@@ -8,7 +8,10 @@
 // AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
 if (!/android|iphone|ipod|series60|symbian|windows ce|blackberry/i.test(navigator.userAgent)) {
 	jQuery(function($) {
-		$("a[rel^='lightbox']").slimbox({/* Put custom options here */}, null, function(el) {
+		$("a[rel^='lightbox']").slimbox(
+		{
+			overlayOpacity : 0// Workaround for bug "Overlay over image"
+		}, null, function(el) {
 			return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
 		});
 	});
