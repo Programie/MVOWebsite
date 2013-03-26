b1c12082fad6d4bbd2305ffaca5dc6b97b984bc5
programie <github@selfcoders.com>
2013-03-26 00:54:25 +0000

    Fixed not caching CSS and JavaScript

--- includes/prehtml/files.php
+++ includes/prehtml/files.php
@@ -3,6 +3,9 @@ switch (Constants::$pagePath[1])
 {
 	case "style.css":
 		header("Content-Type: text/css; charset=utf-8");
+		header("Cache-Control: public, max-age=86400");// 1 day
+		header("Expires: " . gmdate("D, d M Y H:i:s", time() + 86400) . " GMT");// 1 day
+		header("Pragma: ");
 		$path = ROOT_PATH . "/files/css";
 		$dir = scandir($path);
 		foreach ($dir as $file)
@@ -16,6 +19,9 @@ switch (Constants::$pagePath[1])
 		exit;
 	case "script.js":
 		header("Content-Type: text/javascript");
+		header("Cache-Control: public, max-age=86400");// 1 day
+		header("Expires: " . gmdate("D, d M Y H:i:s", time() + 86400) . " GMT");// 1 day
+		header("Pragma: ");
 		$files = explode(" ", Constants::$pagePath[2]);
 		foreach ($files as $file)
 		{
