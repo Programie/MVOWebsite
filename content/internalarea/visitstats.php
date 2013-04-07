eaee6e3b78bc2f3df5ce50fc2fd2d6e6271b83e5
programie <github@selfcoders.com>
2013-04-07 23:49:10 +0000

    Added visual visit stats

---
+++ content/internalarea/visitstats.php
@@ -0,0 +1,48 @@
+<h1>Besucherstatistik</h1>
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>Zeit</th>
+			<th>G&auml;ste</th>
+			<th>Eingeloggte Benutzer</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		$stats = array
+		(
+			"Heute" => "`date` = CURDATE()",
+			"Gestern" => "`date` = DATE_ADD(CURDATE(), INTERVAL -1 DAY)",
+			"Diese Woche" => "YEARWEEK(`date`) = YEARWEEK(NOW())",
+			"Letzte Woche" => "YEARWEEK(`date`) = YEARWEEK(CURDATE() - INTERVAL 7 DAY)",
+			"Diesen Monat" => "YEAR(`date`) = YEAR(NOW()) AND MONTH(`date`) = MONTH(NOW())",
+			"Letzten Monat" => "YEAR(`date`) = YEAR(CURDATE() - INTERVAL 1 MONTH) AND MONTH(`date`) = MONTH(CURDATE() - INTERVAL 1 MONTH)"
+		);
+		foreach ($stats as $name => $whereSql)
+		{
+			$guests = 0;
+			$users = 0;
+			$query = Constants::$pdo->query("SELECT `userId` FROM `visits` WHERE " . $whereSql);
+			while ($row = $query->fetch())
+			{
+				if ($row->userId)
+				{
+					$users++;
+				}
+				else
+				{
+					$guests++;
+				}
+			}
+			echo "
+				<tr>
+					<td>" . $name . "</td>
+					<td>" . $guests . "</td>
+					<td>" . $users . "</td>
+				</tr>
+			";
+		}
+		?>
+	</tbody>
+</table>
