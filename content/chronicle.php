a72bd058c1c5a98ee38d41847d258d6fb43b6088
programie <github@selfcoders.com>
2013-04-13 21:45:50 +0000

    Updated chronicle

--- content/chronicle.php
+++ content/chronicle.php
@@ -1,28 +1,184 @@
 <h1>Chronik</h1>
-<ul>
-	<li>1891: Gr&uuml;ndung der Musikkapelle Reichental</li>
-	<br />
-	<li>1955: Gr&uuml;ndung des Musikverein "Orgelfels" Reichental</li>
-	<br />
-	<li>1962: Anschaffung der 1. Uniform</li>
-	<br />
-	<li>1980: Die ersten Frauen wurden in der Kapelle aufgenommen</li>
-	<br />
-	<li>1984: Das erste Muttertagskonzert fand statt</li>
-	<br />
-	<li>1985: Das neugegr&uuml;ndete Saxofon-Register pr&auml;sentiert sich</li>
-	<br />
-	<li>1988: Ein neuer H&ouml;chststand von 251 Mitgliedern wird verzeichnet</li>
-	<br />
-	<li>1991: 100 Jahre Musikkapelle Reichental</li>
-	<br />
-	<li>1997: Die Jugendkapelle wird gegr&uuml;ndet</li>
-	<br />
-	<li>2000: Mitwirkung bei der SWR4-Sendung "Morgenl&auml;uten"</li>
-	<br />
-	<li>2001: 110 Jahre Musikkapelle Reichental</li>
-	<br />
-	<li>2005: 50 Jahre Musikverein "Orgelfels" Reichental</li>
-	<br />
-	<li>2009: 10 Jahre Dirigentin Diana Jourdan</li>
-</ul>
+
+<table cellspacing="5">
+	<tr>
+		<td>1891:</td>
+		<td>Gr&uuml;ndung der Musikkapelle Reichental</td>
+	</tr>
+	<tr>
+		<td>1955:</td>
+		<td>Gr&uuml;ndung des Musikverein "Orgelfels" Reichental e.V. erster Vorsitzender ist Kornel Merkel</td>
+	</tr>
+	<tr>
+		<td>1962:</td>
+		<td>Anf&uuml;hrung des ersten Rosenmontagsumzugs</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Anschaffung der 1. Uniform</td>
+	</tr>
+	<tr>
+		<td>1974:</td>
+		<td>Erstes Konzert in der Turnhalle mit Bewirtung</td>
+	</tr>
+	<tr>
+		<td>1976:</td>
+		<td>Erster Zeltgottesdienst in Reichental beim Sommerfest des Musikvereins</td>
+	</tr>
+	<tr>
+		<td>1977:</td>
+		<td>Eigene Fastnachtsveranstaltung am Rosenmontag  in der Turnhalle</td>
+	</tr>
+	<tr>
+		<td>1978:</td>
+		<td>Tanzveranstaltung am Fastnachtssonntag zus&auml;tzlich zum Rosenmontag</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Erstes Kurkonzert im Kurpark in Gernsbach</td>
+	</tr>
+	<tr>
+		<td>1980:</td>
+		<td>Die ersten Frauen werden in der Kapelle aufgenommen</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Bis 2010 Beteiligung am Altstadtfest mit Bewirtschaftung, zuerst in der Storrentorstra&szlig;e, dann auf Motex-Parkplatz</td>
+	</tr>
+	<tr>
+		<td>1981:</td>
+		<td>Auftritt  bei der Landesgartenschau in Baden-Baden</td>
+	</tr>
+	<tr>
+		<td>1982:</td>
+		<td>Der Proberaum wird von der Kochschule in ein Klassenzimmer der neuen Schule verlegt</td>
+	</tr>
+	<tr>
+		<td>1984:</td>
+		<td>Jugendausbildung durch eigene Musiker wird begonnen</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Der Verein wird in das Vereinsregister eingetragen und lautet nun: Musikverein "Orgelfels" Reichental e.V.</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Das erste Muttertagskonzert findet statt</td>
+	</tr>
+	<tr>
+		<td>1985:</td>
+		<td>Das neugegr&uuml;ndete Saxofon-Register pr&auml;sentiert sich</td>
+	</tr>
+	<tr>
+		<td>1988:</td>
+		<td>Ein neuer H&ouml;chststand von 251 Mitgliedern wird verzeichnet</td>
+	</tr>
+	<tr>
+		<td>1989:</td>
+		<td>Erster Bunter Abend am Fastnachtssonntag</td>
+	</tr>
+	<tr>
+		<td>1990:</td>
+		<td>Erste Video-Aufzeichnungen von Fastnacht in der Halle</td>
+	</tr>
+	<tr>
+		<td>1991:</td>
+		<td>100 Jahre Musikkapelle Reichental</td>
+	</tr>
+	<tr>
+		<td>1992:</td>
+		<td>Aufnahmen zur Musikkassette "Musikalische Gr&uuml;&szlig;e aus Gernsbach"</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Mitwirkung bei der Fernsehsendung "So ein Tag mit guten Freunden" im Europapark in Rust</td>
+	</tr>
+	<tr>
+		<td>1994:</td>
+		<td>Am 10. Juli erster Dorfhock auf dem Schulhof</td>
+	</tr>
+	<tr>
+		<td>1997:</td>
+		<td>Die Jugendkapelle wird gegr&uuml;ndet, musikalischer Leiter ist Volker Schmitt</td>
+	</tr>
+	<tr>
+		<td>1998:</td>
+		<td>Am 2. Dezember startet die erste Blockfl&ouml;tengruppe, Ausbilder sind Sarah Knapp und Vanessa Kuhs</td>
+	</tr>
+	<tr>
+		<td>1999:</td>
+		<td>Die erste Frau am Dirigentenpult: Diana Koep, heute Jourdan</td>
+	</tr>
+	<tr>
+		<td>2000:</td>
+		<td>Im November findet der erste Jugendaktionstag im Pfarrheim statt bei dem die Jugendarbeit im Verein vorgestellt wird</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Mitwirkung bei der SWR4-Sendung "Morgenl&auml;uten"</td>
+	</tr>
+	<tr>
+		<td>2001:</td>
+		<td>Jubil&auml;umsjahr 110 Jahre Musikkapelle Reichental</td>
+	</tr>
+	<tr>
+		<td>2003:</td>
+		<td>Die Jahreshauptversammlung findet nur noch alle zwei Jahre statt</td>
+	</tr>
+	<tr>
+		<td>2005:</td>
+		<td>Jubil&auml;umsjahr 50 Jahre Musikverein "Orgelfels" Reichental e.V.</td>
+	</tr>
+	<tr>
+		<td>2007:</td>
+		<td>Fastnachtssitzungen werden an zwei Abenden (Samstag und Sonntag) durchgef&uuml;hrt</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>F&ouml;rderverein wird gegr&uuml;ndet.  Gleichberechtigte Vorst&auml;nde sind Esther Klumpp und Ulrike Brasseur</td>
+	</tr>
+	<tr>
+		<td>2009:</td>
+		<td>10 Jahre Dirigentin Diana Jourdan</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>
+			Satzungs&auml;nderung: Der erste und zweite Vorstand werden zu gleichberechtigten Vorst&auml;nden eingetragen.<br />
+			Neues Vorstandsteam werden Erhard Klumpp und Birgit Gerweck.<br />
+			Guido Wieland gibt nach 16 Jahren die Vereinsspitze ab und &uuml;bernimmt von<br />
+			Esther Klumpp die Stelle im Vorstandsteam des F&ouml;rdervereins
+		</td>
+	</tr>
+	<tr>
+		<td>2010:</td>
+		<td>Die Organisation des Rosenmontagsumzugs wird von den Brunnberghexen &uuml;bernommen</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Zum ersten Mal in der Geschichte des Vereins absolviert eine Jungmusikerin, Liliane Brasseur, das Jungmusikerleistungsabzeichen in Gold</td>
+	</tr>
+	<tr>
+		<td>2011:</td>
+		<td>Auftritt beim Neujahrsempfang der Stadt Gernsbach</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>
+			Erstes Herbstfest in der Festhalle mit Vorstellung der Jugendarbeit<br />
+			Als Mittagessen wird Wildschweingulasch aus den heimischen W&auml;ldern serviert
+		</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Erster Auftritt auf der Himmelsb&uuml;hne beim Baden-Badener Weihnachtsmarkt</td>
+	</tr>
+	<tr>
+		<td>2012:</td>
+		<td>Zum 50. Mal f&uuml;hren die Musiker den Rosenmontagsumzug an</td>
+	</tr>
+	<tr>
+		<td></td>
+		<td>Zusammen mit dem Musikverein Staufenberg wird das 43. Bezirkskonzert in der Stadthalle in Gernsbach organisiert</td>
+	</tr>
+</table>
