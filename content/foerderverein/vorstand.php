309563ff9959d908d163721c4b270e592771cfeb
programie <github@selfcoders.com>
2013-08-02 15:16:05 +0000

    Use permissions for displayed users on vorstand pages (/vorstand and /foerderverein/vorstand)

--- content/foerderverein/vorstand.php
+++ content/foerderverein/vorstand.php
@@ -1,16 +1,69 @@
-<h1 class="center">Der Vorstand des F&ouml;rdervereins</h1>
+<?php
+$query = Constants::$pdo->query("SELECT `id`, `firstName`, `lastName` FROM `users` WHERE `enabled` ORDER BY `lastname` ASC, `firstName` ASC");
+while ($row = $query->fetch())
+{
+	$users[] = $row;
+}
+$checkUserInGroup = Constants::$pdo->prepare("SELECT `id` FROM `permissions` WHERE `userId` = :userId AND `permission` = :permission");
 
-<table border="0" align="center" cellspacing="10">
-	<tr>
-		<td>Vorstand (Gleichberechtigt)</td>
-		<td>Ulrike Brasseur, Guido Wieland</td>
-	</tr>
-	<tr>
-		<td>Kassenverwalter</td>
-		<td>Karl Wannenmacher</td>
-	</tr>
-	<tr>
-		<td>Schriftf&uuml;hrer</td>
-		<td>Patrick Wieland</td>
-	</tr>
-</table>
+$groups = array
+(
+	array
+	(
+		"title" => "Vorstand (Gleichberechtigt)",
+		"subPermissions" => array("vorstand")
+	),
+	array
+	(
+		"title" => "Kassenverwalter",
+		"subPermissions" => array("kassenverwalter")
+	),
+	array
+	(
+		"title" => "Schriftf&uuml;hrer",
+		"subPermissions" => array("schriftfuehrer")
+	)
+);
+
+echo "
+	<h1 class='center'>Der Vorstand des F&ouml;rdervereins</h1>
+	<table align='center' cellspacing='10'>
+		<tbody>
+";
+
+foreach ($groups as $group)
+{
+	$userList = array();
+
+	foreach ($group["subPermissions"] as $subPermission)
+	{
+		foreach ($users as $user)
+		{
+			$checkUserInGroup->execute(array
+			(
+				":userId" => $user->id,
+				":permission" => "groups.foerderverein.vorstandschaft." . $subPermission
+			));
+			if ($checkUserInGroup->rowCount())
+			{
+				$userList[] = $user->firstName . " " . $user->lastName;
+			}
+		}
+	}
+
+	if (!empty($userList))
+	{
+		echo "
+			<tr>
+				<td>" . $group["title"] . "</td>
+				<td>" . implode(", ", $userList) . "</td>
+			</tr>
+		";
+	}
+}
+
+echo "
+		</tbody>
+	</table>
+";
+?>
--- content/vorstand.php
+++ content/vorstand.php
@@ -1,38 +1,94 @@
-<h1 class="center">Der Vorstand</h1>
+<?php
+$query = Constants::$pdo->query("SELECT `id`, `firstName`, `lastName` FROM `users` WHERE `enabled` ORDER BY `lastname` ASC, `firstName` ASC");
+while ($row = $query->fetch())
+{
+	$users[] = $row;
+}
+$checkUserInGroup = Constants::$pdo->prepare("SELECT `id` FROM `permissions` WHERE `userId` = :userId AND `permission` = :permission");
 
-<table border="0" align="center" cellspacing="10">
-	<tbody>
-	<tr>
-		<td width="200">Vorstand (Gleichberechtigt)</td>
-		<td>Erhard Klumpp, Birgit Gerweck</td>
-	</tr>
-	<tr>
-		<td>Kassenverwalterin</td>
-		<td>Katrin H&ouml;rth</td>
-	</tr>
-	<tr>
-		<td>Schriftf&uuml;hrerin</td>
-		<td>Heike Kast</td>
-	</tr>
-	<tr>
-		<td>Jugendleiterin</td>
-		<td>Gisela Wieland</td>
-	</tr>
-	<tr>
-		<td>Musikervorstand</td>
-		<td>Karl Fortenbacher, Daniela Zapf</td>
-	</tr>
-	<tr>
-		<td>Jugendvertreter</td>
-		<td>Andreas Wieland, Philipp Wieland</td>
-	</tr>
-	<tr>
-		<td>Aktive Beisitzer</td>
-		<td>David Brasseur, Michael Klumpp, Florian Wieland</td>
-	</tr>
-	<tr>
-		<td>Passive Beisitzer</td>
-		<td>Elke Fortenbacher, Danny Gottschalk, Edith Wieland</td>
-	</tr>
-	</tbody>
-</table>
+$groups = array
+(
+	array
+	(
+		"title" => "Vorstand (Gleichberechtigt)",
+		"subPermissions" => array("vorstand.1", "vorstand.2")
+	),
+	array
+	(
+		"title" => "Kassenverwalterin",
+		"subPermissions" => array("kassenverwalter")
+	),
+	array
+	(
+		"title" => "Schriftf&uuml;hrerin",
+		"subPermissions" => array("schriftfuehrer")
+	),
+	array
+	(
+		"title" => "Jugendleiterin",
+		"subPermissions" => array("jugendleiter")
+	),
+	array
+	(
+		"title" => "Musikervorstand",
+		"subPermissions" => array("musikervorstand")
+	),
+	array
+	(
+		"title" => "Jugendvertreter",
+		"subPermissions" => array("jugendvertreter")
+	),
+	array
+	(
+		"title" => "Aktive Beisitzer",
+		"subPermissions" => array("aktivebeisitzer")
+	),
+	array
+	(
+		"title" => "Passive Beisitzer",
+		"subPermissions" => array("passivebeisitzer")
+	)
+);
+
+echo "
+	<h1 class='center'>Der Vorstand</h1>
+	<table align='center' cellspacing='10'>
+		<tbody>
+";
+
+foreach ($groups as $group)
+{
+	$userList = array();
+
+	foreach ($group["subPermissions"] as $subPermission)
+	{
+		foreach ($users as $user)
+		{
+			$checkUserInGroup->execute(array
+			(
+				":userId" => $user->id,
+				":permission" => "groups.vorstandschaft." . $subPermission
+			));
+			if ($checkUserInGroup->rowCount())
+			{
+				$userList[] = $user->firstName . " " . $user->lastName;
+			}
+		}
+	}
+
+	if (!empty($userList))
+	{
+		echo "
+			<tr>
+				<td>" . $group["title"] . "</td>
+				<td>" . implode(", ", $userList) . "</td>
+			</tr>
+		";
+	}
+}
+
+echo "
+		</tbody>
+	</table>
+";
+?>
