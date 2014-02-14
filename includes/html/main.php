07d718ab07690934dcd0d62b8312d9246de74012
Michael Wieland <github@selfcoders.com>
2014-02-14 02:47:47 +0100

    Fixed indent

--- includes/html/main.php
+++ includes/html/main.php
@@ -25,54 +25,54 @@ if (!$backgroundFile or $_SESSION["backgroundType"] != $backgroundType or !file_
 $backgroundFile = "/files/backgrounds/" . $backgroundType . "/" . $backgroundFile;
 ?>
 <html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title><?php echo implode(PAGE_TITLE_SEPARATOR, $fullPageTitle); ?></title>
-	<?php
-	$path = ROOT_PATH . "/files/css";
-	$md5 = "";
-	$dir = scandir($path);
-	foreach ($dir as $file)
-	{
-		if ($file[0] != "." and is_file($path . "/" . $file))
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title><?php echo implode(PAGE_TITLE_SEPARATOR, $fullPageTitle); ?></title>
+		<?php
+		$path = ROOT_PATH . "/files/css";
+		$md5 = "";
+		$dir = scandir($path);
+		foreach ($dir as $file)
 		{
-			$md5 .= md5_file($path . "/" . $file);
+			if ($file[0] != "." and is_file($path . "/" . $file))
+			{
+				$md5 .= md5_file($path . "/" . $file);
+			}
 		}
-	}
-	?>
-	<link rel="stylesheet" type="text/css" href="/files/style.css?md5=<?php echo md5($md5); ?>"/>
-	<?php
-	$path = ROOT_PATH . "/files/scripts";
-	$md5 = "";
-	$dir = scandir($path);
-	foreach ($dir as $file)
-	{
-		if ($file[0] != "." and is_file($path . "/" . $file))
+		?>
+		<link rel="stylesheet" type="text/css" href="/files/style.css?md5=<?php echo md5($md5); ?>"/>
+		<?php
+		$path = ROOT_PATH . "/files/scripts";
+		$md5 = "";
+		$dir = scandir($path);
+		foreach ($dir as $file)
 		{
-			$md5 .= md5_file($path . "/" . $file);
+			if ($file[0] != "." and is_file($path . "/" . $file))
+			{
+				$md5 .= md5_file($path . "/" . $file);
+			}
 		}
-	}
-	?>
-	<script type="text/javascript" src="/files/script.js?md5=<?php echo md5($md5); ?>"></script>
-	<link rel="icon" href="/files/images/favicon.ico"/>
-</head>
-<body style="background-image: url(<?php echo $backgroundFile; ?>);">
-<div id="unsupported_browser_warning">
-	<p><b>Warnung: Ihr Browser ist nicht auf dem aktuellsten Stand!</b></p>
+		?>
+		<script type="text/javascript" src="/files/script.js?md5=<?php echo md5($md5); ?>"></script>
+		<link rel="icon" href="/files/images/favicon.ico"/>
+	</head>
+	<body style="background-image: url(<?php echo $backgroundFile; ?>);">
+		<div id="unsupported_browser_warning">
+			<p><b>Warnung: Ihr Browser ist nicht auf dem aktuellsten Stand!</b></p>
 
-	<p>Der von Ihnen verwendete Internetbrowser ist veraltet bzw. unterst&uuml;tzt nicht alle auf dieser Seite
-		verwendeten Eigenschaften.</p>
+			<p>Der von Ihnen verwendete Internetbrowser ist veraltet bzw. unterst&uuml;tzt nicht alle auf dieser Seite
+				verwendeten Eigenschaften.</p>
 
-	<p><a href="/update_browser" target="_blank">Klicken Sie hier um weitere Informationen zu erhalten.</a></p>
-</div>
-<div id="container">
-	<?php require_once "header.php"; ?>
-	<?php require_once "body.php"; ?>
-	<?php require_once "footer.php"; ?>
-</div>
-<div id="overlay_container">
-	<div id="backtotop" title="Nach oben"></div>
-	<div id="print" title="Drucken"></div>
-</div>
-</body>
+			<p><a href="/update_browser" target="_blank">Klicken Sie hier um weitere Informationen zu erhalten.</a></p>
+		</div>
+		<div id="container">
+			<?php require_once "header.php"; ?>
+			<?php require_once "body.php"; ?>
+			<?php require_once "footer.php"; ?>
+		</div>
+		<div id="overlay_container">
+			<div id="backtotop" title="Nach oben"></div>
+			<div id="print" title="Drucken"></div>
+		</div>
+	</body>
 </html>
