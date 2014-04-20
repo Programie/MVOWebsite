643df41b15395ea662fca279476d87dfdbfe7e0d
Michael Wieland <github@selfcoders.com>
2014-04-20 23:18:23 +0200

    Added picture box page

---
+++ content/internalarea/picturebox.php
@@ -0,0 +1,10 @@
+<?php
+if (Constants::$accountManager->hasPermission("picturebox.upload"))
+{
+	// TODO: Allow user to upload multiple files
+}
+
+if (Constants::$accountManager->hasPermission("picturebox.manage"))
+{
+	// TODO: Allow user to manage files (Delete)
+}
