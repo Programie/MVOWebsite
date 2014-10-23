08b944613d1a77cf6ca2f87d2fbc8919e59dac58
Michael Wieland <github@selfcoders.com>
2014-10-24 00:09:36 +0200

    Updated jugendausbildung/verein

--- content/jugendausbildung/verein.php
+++ content/jugendausbildung/verein.php
@@ -3,28 +3,28 @@
 <p>
 	<b>Jugendausbildung</b><br/>
 	Die Jugendausbildung hat bei uns im Verein schon immer einen sehr hohen Stellenwert, denn nur durch eine gesunde
-	Nachwuchsf&ouml;rderung ist das zuk&uuml;nftige Bestehen unseres Vereins gesichert.<br/><br/>
+	Nachwuchsf&ouml;rderung ist das zuk&uuml;nftige Bestehen unseres Vereins gesichert.<br/>
+	<br/>
 	Das Ziel unserer Ausbildung ist die aktive Mitwirkung in der Jugend- bzw. Gesamtkapelle des Musikvereins
 	"Orgelfels" Reichental (MVO). Voraussetzung f&uuml;r die Mitwirkung in der Gesamtkapelle ist der Erwerb des
 	Jungmusikerleistungsabzeichens (JMLA) in Bronze.
 <p>
 	<b>Blockfl&ouml;tenunterricht</b><br/>
 	F&uuml;r Kinder ab der ersten Klasse bieten wir Blockfl&ouml;tenunterricht in kleinen Gruppen an. Der Unterricht
-	erfolgt durch vereinsangeh&ouml;rige Ausbilder (Qualifikation JMLA Silber) und dauert je nach Gruppengr&ouml;&szlig;e
-	30 oder 45 Minuten pro Woche. Die Kinder erlernen spielerisch erste Notenkenntnisse, Rhythmik, theoretisches
-	Grundwissen und das Zusammenspiel.
+	erfolgt durch vereinseigene Musiker/innen und dauert 45 Minuten pro Woche. Die Kinder erlernen spielerisch erste
+	Notenkenntnisse, Rhythmik, theoretisches Grundwissen und das Zusammenspiel.
 </p>
 
 <p>
 	<b>Instrumentalunterricht</b><br/>
 	Nach der Blockfl&ouml;tenausbildung erfolgt die Ausbildung auf einem Blas- oder Schlaginstrument. Die erlernten
 	Kenntnisse und F&auml;higkeiten aus dem Blockfl&ouml;tenunterricht erleichtern dabei den Einstieg in die
-	Instrumentalausbildung.<br/><br/>
-	Die Ausbildung erfolgt, soweit dies m&ouml;glich ist, durch vereinsangeh&ouml;rige Musiker/innen, die die
-	entsprechende Qualifikation haben (C-2 Ausbilder-Lehrgang). Bei Bedarf werden weitere externe Lehrkr&auml;fte
-	oder die Musikschule Murgtal eingebunden.<br/><br/>
-	Damit erm&ouml;glichen wir den Kindern und Jugendlichen eine fundierte Ausbildung und bereiten sie auf das
-	Gesamtspiel in der Kapelle vor.
+	Instrumentalausbildung.<br/>
+	<br/>
+	Die Ausbildung erfolgt, soweit dies m&ouml;glich ist, durch vereinseigene Musiker/innen mit entsprechender Qualifikation.
+	Bei Bedarf werden weitere externe Lehrkr&auml;fte oder die Musikschule Murgtal eingebunden.<br/>
+	<br/>
+	Damit erm&ouml;glichen wir den Kindern und Jugendlichen eine fundierte Ausbildung und bereiten sie auf das Gesamtspiel in der Kapelle vor.
 </p>
 
 <p>
