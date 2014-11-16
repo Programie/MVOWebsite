16c71459c8e16caa013e87d1331a8e3fc3feaaaa
Programie <github@selfcoders.com>
2014-11-16 18:29:10 +0100

    Show session count in visit stats

--- content/internalarea/visitstats.php
+++ content/internalarea/visitstats.php
@@ -10,7 +10,15 @@
 	</thead>
 	<tbody>
 	<?php
-	$stats = array("Heute" => "`date` = CURDATE()", "Gestern" => "`date` = DATE_ADD(CURDATE(), INTERVAL -1 DAY)", "Diese Woche" => "YEARWEEK(`date`, 1) = YEARWEEK(NOW(), 1)", "Letzte Woche" => "YEARWEEK(`date`, 1) = YEARWEEK(CURDATE() - INTERVAL 7 DAY, 1)", "Diesen Monat" => "YEAR(`date`) = YEAR(NOW()) AND MONTH(`date`) = MONTH(NOW())", "Letzten Monat" => "YEAR(`date`) = YEAR(CURDATE() - INTERVAL 1 MONTH) AND MONTH(`date`) = MONTH(CURDATE() - INTERVAL 1 MONTH)");
+	$stats = array
+	(
+		"Heute" => "`date` = CURDATE()",
+		"Gestern" => "`date` = DATE_ADD(CURDATE(), INTERVAL -1 DAY)",
+		"Diese Woche" => "YEARWEEK(`date`, 1) = YEARWEEK(NOW(), 1)",
+		"Letzte Woche" => "YEARWEEK(`date`, 1) = YEARWEEK(CURDATE() - INTERVAL 7 DAY, 1)",
+		"Diesen Monat" => "YEAR(`date`) = YEAR(NOW()) AND MONTH(`date`) = MONTH(NOW())",
+		"Letzten Monat" => "YEAR(`date`) = YEAR(CURDATE() - INTERVAL 1 MONTH) AND MONTH(`date`) = MONTH(CURDATE() - INTERVAL 1 MONTH)"
+	);
 	foreach ($stats as $name => $whereSql)
 	{
 		$guests = 0;
@@ -38,3 +46,17 @@
 	?>
 	</tbody>
 </table>
+
+<h2>Aktive Besucher</h2>
+<?php
+$query = Constants::$pdo->query("SELECT COUNT(`date`) AS `count` FROM `sessions`");
+$count = $query->fetch()->count;
+
+if ($count == 1)
+{
+		echo "<p>Derzeit bist nur du online.</p>";
+}
+else
+{
+	echo "<p>Derzeit sind " . $count . " Besucher online.</p>";
+}
