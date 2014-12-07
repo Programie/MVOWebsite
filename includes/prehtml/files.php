8130ce3d9e458762be89dbefa93ed4737cf74ce7
Michael Wieland <github@selfcoders.com>
2014-12-07 23:49:31 +0100

    Optimized CSS

--- includes/prehtml/files.php
+++ includes/prehtml/files.php
@@ -6,13 +6,34 @@ switch (Constants::$pagePath[1])
 		header("Cache-Control: public, max-age=86400"); // 1 day
 		header("Expires: " . gmdate("D, d M Y H:i:s", time() + 86400) . " GMT"); // 1 day
 		header("Pragma: ");
+
 		$path = ROOT_PATH . "/files/css";
-		$dir = scandir($path);
-		foreach ($dir as $file)
+
+		$files = array
+		(
+			"alerts",
+			"colorbox",
+			"dropzone",
+			"elusive-webfont",
+			"fullcalendar",
+			"jcrop",
+			"jquery-ui",
+			"jstree",
+			"photobox",
+			"polaroids",
+			"timepicker",
+			"global",
+			"main",
+			"internal-area",
+			"misc"
+		);
+
+		foreach ($files as $file)
 		{
-			if ($file[0] != "." and is_file($path . "/" . $file))
+			$file = $path . "/" . $file . ".css";
+			if (is_file($file))
 			{
-				readfile($path . "/" . $file);
+				readfile($file);
 				echo "\n";
 			}
 		}
@@ -22,9 +43,38 @@ switch (Constants::$pagePath[1])
 		header("Cache-Control: public, max-age=86400"); // 1 day
 		header("Expires: " . gmdate("D, d M Y H:i:s", time() + 86400) . " GMT"); // 1 day
 		header("Pragma: ");
-		$javaScriptFiles = array("errorreport", "jquery", "jquery-ui", "globalize", "globalize-de", "datepicker-de", "metadata", "jcrop", "colorbox", "photobox", "tablesorter", "tablesorter-widgets", "noty", "noty-layout", "noty-theme", "timepicker", "timepicker-de", "jquery.form", "jquery.contextmenu", "jstree", "fullcalendar", "dropzone", "google-analytics", "general");
+
 		$path = ROOT_PATH . "/files/scripts";
-		foreach ($javaScriptFiles as $file)
+
+		$files = array
+		(
+			"errorreport",
+			"jquery",
+			"jquery-ui",
+			"globalize",
+			"globalize-de",
+			"datepicker-de",
+			"metadata",
+			"jcrop",
+			"colorbox",
+			"photobox",
+			"tablesorter",
+			"tablesorter-widgets",
+			"noty",
+			"noty-layout",
+			"noty-theme",
+			"timepicker",
+			"timepicker-de",
+			"jquery.form",
+			"jquery.contextmenu",
+			"jstree",
+			"fullcalendar",
+			"dropzone",
+			"google-analytics",
+			"general"
+		);
+
+		foreach ($files as $file)
 		{
 			$file = $path . "/" . $file . ".js";
 			if (is_file($file))
