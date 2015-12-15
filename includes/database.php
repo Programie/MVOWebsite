d28eca2acf06f69f952be69cfeb5db5e6a430db2
Michael Wieland <github@selfcoders.com>
2015-12-15 23:03:30 +0100

    Fixed not throwing exceptions on database errors

--- includes/database.php
+++ includes/database.php
@@ -2,10 +2,7 @@
 try
 {
 	Constants::$pdo = new PDO(MYSQL_DSN, MYSQL_USERNAME, MYSQL_PASSWORD);
-	if (ENVIRONMENT == "dev")
-	{
-		Constants::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
+	Constants::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	Constants::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 	Constants::$pdo->query("SET NAMES utf8");
 }
